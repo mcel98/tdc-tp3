d9536039e5a2a55f970bfb9d9f95f0fcbf0051fc
mariano ezequiel segura maag <msegura@dc.uba.ar>
2020-11-08 14:14:23 -0300

    fix de mensajes

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ def UDPScanner(ip,port,ttl):
         for item in retrans:
             if (str(type(item)) !=" < type ‘NoneType’ > "):
                 UDPScanner(ip, port, ttl)
-                return "Abierto | Filtrado"
-            elif(pudp.haslayer(UDP)):
-                return "Abierto"
-            elif (pudp.haslayer(ICMP)):
-                if (int(pudp.getlayer(ICMP).type) == 3 and int(pudp.getlayer(ICMP).code) == 3):
-                    return "Cerrado"
-                elif (int(pudp.getlayer(ICMP).type) == 3 and int(pudp.getlayer(ICMP).code) in [1, 2, 9, 10,13]):
-                    return "Filtrado"
-            else:
-                return "CHECK"
+            return "Abierto | Filtrado"
+    elif(pudp.haslayer(UDP)):
+            return "Abierto"
+    elif(pudp.haslayer(ICMP)):
+            if (int(pudp.getlayer(ICMP).type) == 3 and int(pudp.getlayer(ICMP).code) == 3):
+                return "Cerrado"
+            elif (int(pudp.getlayer(ICMP).type) == 3 and int(pudp.getlayer(ICMP).code) in [1, 2, 9, 10,13]):
+                return "Filtrado"
+    else:
+        return "CHECK"
 
 ports = [port for port in range(1, 1025)]
 
@@ -38,4 +38,4 @@ for i in ports:
             sr1(IP(dst=ip) / TCP(dport=ports, flags='AR'), verbose = False, timeout = 1)
         elif 0x14 == tcp_layer.flags:
             print(" cerrado", tcp_layer.flags)
-    UDPScanner(ip,i,10)
+    print(UDPScanner(ip,i,10))
